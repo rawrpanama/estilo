abbdade2a04b294145bd1aacd3df94117249cb03
rawrpanama <rawr.panama@hotmail.com>
2015-04-24 21:10:21 -0600

    Revert "3D add"
    This reverts commit 2e84b930615833f476446109268a4201ff96a223.

--- 3D/mueble5.js
+++
@@ -1,8 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.onload = function(){
-    if(xhr.status === 200){
-        document.getElementById('content').innerHTML = xhr.responseText;
-    }
-};
-xhr.open('POST', 'ajax/datos.html', true);
-xhr.send(null);
